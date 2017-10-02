a91f474ef00dd3adec8583da8ae848740104ae2e
Remco Gaikema <hi@remcogaikema.com>
2017-10-02 23:07:41 +0200

    started with displaying canvas files

--- site/templates/canvas.php
+++ site/templates/canvas.php
@@ -1,7 +1,50 @@
 <?php 
 	include("./header.inc"); 
 
-	echo ' hier komen de velden van de geuploade onderdelen van de canvas';
+?>
 
+	<div class="screen" id="canvas_screen">
+			      
+	    <?php  
+
+			foreach($page->canvas_items as $canvas_item) {
+
+				if ($canvas_item->canvas_type_check == 1) {
+
+					$img = $canvas_item->canvas_image_file;
+
+					var_dump($canvas_item->canvas_image_kind);
+
+					if ($canvas_item->canvas_image_kind == 1) {
+
+					echo '<img class="c_img landscape" src="' . $img->url . '" alt="' . $img->description . '">'; 
+
+					}elseif ($canvas_item->canvas_image_kind == 2) {
+						echo "panorama";
+					}
+				}
+				elseif ($canvas_item->canvas_type_check == 2){
+					echo "video";
+				}
+				elseif ($canvas_item->canvas_type_check == 3){
+					echo "button";
+				}
+				else {
+					echo "onbekend item";
+				}
+			} 
+		?>
+	</div>
+
+	<script>
+		$(function(o){	
+			o = $("#canvas_screen").overscroll({
+
+			});
+
+		});
+    </script>
+
+<?php
 	include("./footer.inc"); 
 ?>
